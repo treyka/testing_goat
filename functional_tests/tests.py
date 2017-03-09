36222406865df682c56cd4152ed3e3dedc88ec4c
Trey Darley <trey@kingfisherops.com>
2017-03-09 10:03:29 +0000

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,6 +8,16 @@ import time
 MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -29,7 +40,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices that the input box is nicely centered
@@ -59,7 +70,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -107,7 +118,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith can start a new list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
@@ -128,7 +139,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # Francis visit the home page. There is no sign of Edith's
         # list
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
